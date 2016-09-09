fae40196487c2cf296100f069557f53a38751969
shailesh2088 <shailesh2088@gmail.com>
2016-09-08 21:26:19 -0700

    Completed image guessing game + other samples from chapter 9

--- image-game/image-guess.js
+++ image-game/image-guess.js
@@ -3,7 +3,8 @@ window.onload = init;
 function init() {
 	var images = document.getElementsByTagName("img");
 	for (var i = 0; i< images.length; i++) {
-		images[i].onclick = showAnswer;
+		images[i].onmouseover = showAnswer;
+		images[i].onmouseout = reblur;
 	}
 }
 
@@ -11,5 +12,12 @@ function showAnswer(e) {
 	var image = e.target;
 	var name = image.id;
 	name = name + ".jpg";
+	image.src = name;
+}
+
+function reblur (e) {
+	var image = e.target;
+	var name = image.id;
+	name = name + "blur.jpg";
 	image.src = name;
 }
